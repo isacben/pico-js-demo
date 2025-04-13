09ab9bf8545870d0158fade2641b8911fa141d68
Isaac Benitez <ibsandoval@gmail.com>
2025-04-12 21:59:13 -0700

    this is flappy bird now

--- game.js
+++ game.js
@@ -1,74 +1,77 @@
 let posX = 0;
 let inc = 1;
-let playerX = 40;
-let playerY = 80;
+let birdY = 20;
+
+let velocity = 0;
+let gravity = 0.1;
 
 let T = 0;
-const playerAnimation = [0, 1];
+const playerAnimation = [0, 1, 2];
 
 function _update() {
     T += 1;
-    posX += inc;
-
-    if (posX >= 120) inc = -1;
-    if (posX <= 0) inc = 1;
-
-    if (btn(0)) {
-        playerX -= 1;
-    }
-
-    if (btn(1)) {
-        playerX += 1;
-    }
-
-    if (btn(2)) {
-        playerY -= 1;
-    }
-
-    if (btn(3)) {
-        playerY += 1;
-    }
 }
 
 function _draw() {
-    cls();
-    custom_function(posX);
+    cls(12);
+    animateBird();
+
+    fall();
+    flap();
     print("hello world!", 40, 60); 
 }
 
-function custom_function(x) {
-    rect(0, 0, 8, 8);
-    rectfill(8, 0, 8, 8);
-    circ(40, x, 3);
-    circfill(x, 28, 8, 4);
-    line(x, 28, 40, x);
-    line(10, 10, 20, 30, 9);
-    const sp = Math.floor(T / 28) % playerAnimation.length;
-    spr(playerAnimation[sp], playerX, playerY);
+function animateBird() {
+    const sp = Math.floor(T / 6) % playerAnimation.length;
+    spr(playerAnimation[sp], 20, birdY);
+}
 
+function flap() {
+    if (btnp(2)) {
+        velocity = -2;
+    }
+}
+function fall() {
+    velocity += gravity;
+    birdY += velocity;
+
+    if (birdY >= 121) {
+        birdY = 121;
+        velocity = 0;
+    }
 }
 
 // Sprites
 
 const sprites = {
     0: [
-        [,3,3,3,3,3,4,],
-        [,,3,5,15,5,,4],
-        [,,3,15,15,15,,15],
-        [,3,3,3,3,3,3,4],
-        [,15,4,4,4,4,,4],
-        [,,3,3,3,3,,4],
-        [,,3,,,3,4,],
+        [],
+        [,,,9,7,7,7,],
+        [,9,9,9,7,7,5],
+        [7,7,7,9,9,7,7,],
+        [7,7,7,7,9,8,8,8],
+        [7,7,7,7,9,9,9,],
+        [,7,7,9,9,9,,],
         []
     ],
     1: [
-        [,,,,,,4,],
-        [,3,3,3,3,3,,4],
-        [,,3,5,15,5,,15],
-        [,,3,15,15,15,3,4],
-        [,3,4,4,4,4,,4],
-        [,15,3,3,3,3,,4],
-        [,,3,,,3,4,],
+        [],
+        [,,,9,7,7,7,],
+        [,9,9,9,7,7,5],
+        [9,9,9,9,9,7,7,],
+        [7,7,7,7,9,8,8,8],
+        [7,7,7,7,9,9,9,],
+        [,9,9,9,9,9,,],
+        []
+    ],
+    2: [
+        [],
+        [,,,9,7,7,7,],
+        [,9,9,9,7,7,5],
+        [9,9,9,9,9,7,7,],
+        [9,7,7,7,9,8,8,8],
+        [7,7,7,9,9,9,9,],
+        [7,7,9,9,9,9,,],
         []
     ]
 }
